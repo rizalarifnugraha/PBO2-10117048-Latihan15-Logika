fb77f46f8585f98bc1911f76749a839d1f842843
rizalarifnugraha <43487664+rizalarifnugraha@users.noreply.github.com>
2018-09-22 20:14:01 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117048/latihan15/logika/PBO210117048Latihan15Logika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * Nama  : Rizal Arif Nugraha
+ * Kelas : PBO2 
+ * NIM   : 10117048
+ * Deskripsi Program : Program ini berisi program untuk menggunakan logika false 
+ * or true
+
+ */
+public class PBO210117048Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("(a && b) = " + !(a && b));
+    }
+    
+}
